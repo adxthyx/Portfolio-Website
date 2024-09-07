f3c35487be63bab0f6d568aee7406aa448afc8e4
adxthyx <adithyanarayana02@gmail.com>
2024-09-08 02:28:40 +0530

    Deploy

--- src/app/components/EmailSection1.jsx
+++ src/app/components/EmailSection1.jsx
@@ -9,35 +9,17 @@ import Image from "next/image";
 const EmailSection = () => {
   const [emailSubmitted, setEmailSubmitted] = useState(false);
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const data = {
-      email: e.target.email.value,
-      subject: e.target.subject.value,
-      message: e.target.message.value,
-    };
-    const JSONdata = JSON.stringify(data);
-    const endpoint = "/api/send";
+    const email = e.target.email.value;
+    const subject = e.target.subject.value;
+    const message = e.target.message.value;
 
-    // Form the request for sending data to the server.
-    const options = {
-      // The method is POST because we are sending data.
-      method: "POST",
-      // Tell the server we're sending JSON.
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // Body of the request is the JSON data we created above.
-      body: JSONdata,
-    };
+    const mailtoLink = `https://mail.google.com/mail/?view=cm&fs=1&to=adithyanarayana02@gmail.com&su=${encodeURIComponent(
+      subject
+    )}&body=${encodeURIComponent(message)}`;
 
-    const response = await fetch(endpoint, options);
-    const resData = await response.json();
-
-    if (response.status === 200) {
-      console.log("Message sent.");
-      setEmailSubmitted(true);
-    }
+    window.location.href = mailtoLink;
   };
 
   return (
@@ -46,7 +28,7 @@ const EmailSection = () => {
       className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
     >
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
-      <div className="z-10">
+      <div className="z-1">
         <h5 className="text-xl font-bold text-white my-2">
           Let&apos;s Connect
         </h5>
@@ -57,13 +39,13 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
+          <Link href="https://github.com/adxthyx">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
           <Link href="https://leetcode.com/u/adxthyx/">
             <Image src={LeetcodeIcon} alt="Leecode Icon" height={38} />
-            </Link>
-          <Link href="linkedin.com">
+          </Link>
+          <Link href="www.linkedin.com/in/adxthyx">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
@@ -104,7 +86,7 @@ const EmailSection = () => {
                 id="subject"
                 required
                 className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="Subject"
+                placeholder="Just saying hi"
               />
             </div>
             <div className="mb-6">

b44ad286546089500de7ca031574e942145079fb
adxthyx <adithyanarayana02@gmail.com>
2024-09-11 18:33:32 +0530

    Final

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -57,13 +57,13 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
+          <Link href="https://github.com/adxthyx">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
           <Link href="https://leetcode.com/u/adxthyx/">
             <Image src={LeetcodeIcon} alt="Leecode Icon" height={38} />
             </Link>
-          <Link href="linkedin.com">
+          <Link href="https://www.linkedin.com/in/adxthyx/">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -7,8 +7,8 @@ import { motion, useInView } from "framer-motion";
 const projectsData = [
   {
     id: 1,
-    title: "React Portfolio Website",
-    description: "Project 1 description",
+    title: "NextJS Portfolio Website",
+    description: "Developed a NextJS portfolio website and deployed it using Vercel ",
     image: "/images/projects/1.png",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/adxthyx/Portfolio-Website/",
@@ -17,7 +17,7 @@ const projectsData = [
   {
     id: 2,
     title: "Multicity Vehicle Routing",
-    description: "Project 2 description",
+    description: "Implemented and optimized multicity vehicle routing solution resulting in  reduction in overall travel costs and carbon footprint.Utilized advanced data structures and algorithms resulting in  decrease in travel time across multiple cities.Integrated real-time traffic analysis data leading to improvement in route accuracy and time saving",
     image: "/images/projects/2.png",
     tag: ["All", "Algorithms"],
     gitUrl: "https://github.com/gowdavidwan2003/Efficient-Vehicle-Routing",
@@ -26,7 +26,7 @@ const projectsData = [
   {
     id: 3,
     title: "PaaS Product for NGO",
-    description: "Project 3 description",
+    description: "Built a full-stack Next.js PaaS for NGOs to find investors using semantic search.Integrated a SaaS tool with OpenAI and Python-PPTX to create dynamic pitch presentations.Added a dashboard to track activities and connect NGOs with investors.",
     image: "/images/projects/3.png",
     tag: ["All", "ML"],
     gitUrl: "https://github.com/Parth-G27/Tortoise",
@@ -35,7 +35,7 @@ const projectsData = [
   {
     id: 4,
     title: "Assistive Device for Blind using VLM",
-    description: "Project 4 description",
+    description: "Finetuned a VLM model named MoonDream and BLIP and utilized this model to a mobile architecture incluing webam,earphone to assist blind in realtime ",
     image: "/images/projects/4.png",
     tag: ["All", "ML"],
     gitUrl: "https://github.com/adxthyx/Frontend",
@@ -44,7 +44,7 @@ const projectsData = [
   {
     id: 5,
     title: "Regional Language Character Recognition",
-    description: "Project 4 description",
+    description: "Coordinated with my classmates to build a model to recognize handwritten kannada languauge character and developed a interactive website for children to learn the languauge with gamified experience",
     image: "/images/projects/5.png",
     tag: ["All", "ML"],
     gitUrl: "https://github.com/adxthyx/Frontend",
@@ -53,7 +53,7 @@ const projectsData = [
   {
     id: 6,
     title: "Attendance Tracker/Face Recognition",
-    description: "Project 5 description",
+    description: "Built a face recognition ML model using VGGFace and utilized HaarCascade to recognize every single person in the image and keep track of attendance of the class dynamically",
     image: "/images/projects/6.png",
     tag: ["All", "Web","ML"],
     gitUrl: "https://github.com/adxthyx/Attendance--Face_Recognition",
@@ -62,7 +62,7 @@ const projectsData = [
   {
     id: 7,
     title: "Minimalistic Protal for RIT student with integrated Chatbot",
-    description: "Authentication and CRUD operations",
+    description: "Developed a Minimalist student portal for RIT students with integrated Chatbot.The portal utilizes data extraction using BeautifulSoup and auto logins using Selenium.The chatbot is connected to Database and can be used to get relevant information about grades,attendance,events and timetables.",
     image: "/images/projects/7.png",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/adxthyx/CHAT_MSRIT",
@@ -71,7 +71,7 @@ const projectsData = [
   {
     id: 8,
     title: "PPT Generator",
-    description: "Project 4 description",
+    description: "Created a project of automatic PPT generation which takes user input like Topic,No of slides and additional content and generate a PPT using Python-PPTX and Open AI API",
     image: "/images/projects/8.png",
     tag: ["All", "ML"],
     gitUrl: "https://github.com/adxthyx/PPT_Generator",
@@ -81,7 +81,7 @@ const projectsData = [
   {
     id: 9,
     title: "Emergency Fire Protocol",
-    description: "Project 4 description",
+    description: "Created a webapp for systamatic protocol to follow in fire hazard situation in urban areas,Used YOLO v5 and MoonDream 2.0 to accurately recognize fire and then used",
     image: "/images/projects/9.png",
     tag: ["All", "ML"],
     gitUrl: "https://github.com/adxthyx/Emergency_Fire_Protocol",
